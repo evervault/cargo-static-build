df7187195c224e067ede328b4f66af6ec993ee32
Liam Farrelly <liam@evervault.com>
2021-06-21 18:08:21 +0100

    Sudo before linking in entrypoint
    Runner uses passwordless sudo, [from docs](https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#administrative-privileges-of-github-hosted-runners)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
 # hack, move home to $HOME(/github/home)
-ln -s /root/.cargo $HOME/.cargo
-ln -s /root/.rustup $HOME/.rustup
+sudo ln -s /root/.cargo $HOME/.cargo
+sudo ln -s /root/.rustup $HOME/.rustup
 
 # go to the repo root
 cd $GITHUB_WORKSPACE
